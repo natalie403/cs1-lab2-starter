156b54fa2dd333f97832873f7987ea6656516aed
natalie403 <natvu314@gmail.com>
2025-03-05 21:33:54 -0800

    Main

--- main.cpp
+++ main.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
 
+int main() {
+
+// Part 1: Vertical Output
+
+    float x, y, z;
+        x = 3.00;
+        y = 15.73;
+        z = x + y;
+
+    cout << fixed << setprecision(2) << setw(7);
+
+    cout << x << endl;
+    cout << "+ " << y << endl;
+    cout << "-------" << endl;
+    cout << "  " << z << endl;
+
+// Part 2: Interactive Input
+
+    float a, b, c;
+
+    cout << "Enter a number: ";
+    cin >> a;
+    cout << "Enter a number: ";
+    cin >> b;
+
+    c = a + b;
+
+    cout << setw(7);
+    cout << a << endl;
+    cout << "+ " << b << endl;
+    cout << "-------" << endl;
+    cout << " " << c << endl;
+
+    return 0;
+
+}
